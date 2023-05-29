d9b5ae9ea302345261b269580834a6c6fc9ef590
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-28 20:17:49 -0400

    Variables, Swap Two Variables, User Input, Expression, and GUI Intro

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,6 +1,64 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I love pizza");
-        System.out.println("I love chicken");
+//        Variables
+
+//        int x = 123;
+//        float y = 3.14f;
+//        boolean z = true;
+//        char symbol = '@';
+//        String name = "Luis";
+//        System.out.println(name);
+//        --------------------------------------------------------------------------------------------------------------
+//        Swap two variables
+
+//        String x = "Water";
+//        String y = "Kool-Aid";
+//        String temporary;
+//
+//        temporary = x;
+//        x = y;
+//        y = temporary;
+//
+//        System.out.println("x: " + x);
+//        System.out.println("x: " + y);
+//        --------------------------------------------------------------------------------------------------------------
+//        User input
+
+//        Scanner scanner = new Scanner(System.in);
+
+//        System.out.println("What is your name? ");
+//        String name = scanner.nextLine();
+
+//        System.out.println("How old are you? ");
+//        int age = scanner.nextInt();
+//        scanner.nextLine();
+
+//        System.out.println("What is your favorite food?");
+//        String food = scanner.nextLine();
+//
+//        System.out.println("Hello "+name);
+//        System.out.println("You are "+age+" years old");
+//        System.out.println("You like"+food);
+//        --------------------------------------------------------------------------------------------------------------
+//        Expression = operands & operators
+//        Operands = values, variables, numbers, quantity
+//        Operators = + - * / %
+
+//        int friends = 10;
+//        friends = friends + 1;
+//        System.out.println(friends);
+//        --------------------------------------------------------------------------------------------------------------
+//        GUI intro
+//        String name = JOptionPane.showInputDialog("Enter your name");
+//        JOptionPane.showMessageDialog(null, "Hello "+name);
+//
+//        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+//        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+//
+//        Double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+//        JOptionPane.showMessageDialog(null, "You are"+height+" cm tall");
     }
 }
