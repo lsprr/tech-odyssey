ac8cdfddeb72df690c51a89807a2aed30189b425
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-06 17:29:15 -0400

    Logical Operators

--- java/src/Main.java
+++ java/src/Main.java
@@ -140,6 +140,26 @@ public class Main {
 //                System.out.println("That is not a day!");
 //        }
 //        --------------------------------------------------------------------------------------------------------------
+//        Logical Operators
 
+//        int temp = 15;
+//
+//        if (temp > 30) {
+//            System.out.println("It is hot outside");
+//        } else if (temp >= 20 && temp <= 30) {
+//            System.out.println("It is warm outside");
+//        } else {
+//            System.out.println("It is cold outside");
+//        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You are playing a game! Press q or Q to quit");
+        String response = scanner.next();
+
+        if (!response.equals("q") || !response.equals("Q")) {
+            System.out.println("You are still playing the game *pew pew*");
+        } else {
+            System.out.println("You quit the game");
+        }
     }
 }
