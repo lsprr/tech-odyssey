44b3539f6fbf7852e02d753cc01667f8a298b66a
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-07 17:49:42 -0400

    Constructors

--- java/src/Main.java
+++ java/src/Main.java
@@ -472,6 +472,15 @@ public class Main {
 //        System.out.println(myCar1.model);
 //        myCar1.drive();
 //        --------------------------------------------------------------------------------------------------------------
+//        Constructors
+//
+//        Special method that is called when an object is instantiated (created)
+//
+//        Please check Human.java in src/
+//        Human human = new Human("Rick", 65, 70);
+//        Human human2 = new Human("Morty", 16, 50);
+//        System.out.println(human2.name);
+//        --------------------------------------------------------------------------------------------------------------
     }
 }
 
