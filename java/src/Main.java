e7c21c96d6003a21bb633d6cbd7a32621eb2b5da
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-06 20:12:52 -0400

    Nested Loops

--- java/src/Main.java
+++ java/src/Main.java
@@ -184,5 +184,30 @@ public class Main {
 //            System.out.println(i);
 //        }
 //        --------------------------------------------------------------------------------------------------------------
+//        Nested Loops
+//
+//        Scanner scanner = new Scanner(System.in);
+//        int rows;
+//        int columns;
+//        String symbol = "";
+//
+//        System.out.println("Enter # of rows: ");
+//        rows = scanner.nextInt();
+//
+//        System.out.println("Enter # of columns: ");
+//        columns = scanner.nextInt();
+//
+//        System.out.println("Enter symbol to use: ");
+//        symbol = scanner.next();
+//
+//        for (int i = 1; i <= rows; i++) {
+//            System.out.println();
+//
+//            for (int j = 1; j <= columns; j++) {
+//                System.out.print(symbol);
+//            }
+//        }
+//        --------------------------------------------------------------------------------------------------------------
+
     }
 }
