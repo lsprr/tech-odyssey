c322a6f8e09205179d435c0fced828d63859761e
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-06 21:18:37 -0400

    Wrapper Classes

--- java/src/Main.java
+++ java/src/Main.java
@@ -275,5 +275,29 @@ public class Main {
 //
 //        System.out.println(result);
 //        --------------------------------------------------------------------------------------------------------------
+//        Wrapper
+//
+//        It provides a way to use primitive data types as reference data types reference data types contain useful
+//        methods can be used with collections (Ex.ArrayList)
+//
+//        Autoboxing = the automatic conversion that the Java complier makes between the primitive types
+//        and their corresponding object wrapper class
+//
+//        Unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive
+//
+//        primitive | wrapper
+//        -------------------
+//        boolean   | Boolean
+//        char      | Character
+//        int       | Integer
+//        double    | Double
+//
+//        Boolean a = true;
+//        Character b = "@";
+//        Integer c = 123;
+//        Double d = 3.14;
+//        String e = "Luis";
+//        --------------------------------------------------------------------------------------------------------------
+
     }
 }
