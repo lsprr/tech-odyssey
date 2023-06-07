6c1c7e52cf0290f1e7c7e51d63551113534060d7
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-06 20:26:28 -0400

    Arrays

--- java/src/Main.java
+++ java/src/Main.java
@@ -208,6 +208,23 @@ public class Main {
 //            }
 //        }
 //        --------------------------------------------------------------------------------------------------------------
-
+//        Array
+//
+//        String[] cars = {"Camaro", "Corvette", "Tesla"};
+//        cars[0] = "Mustang";
+//        System.out.println(cars);
+//
+//        String[] cars = new String[3];
+//
+//        cars[0] = "Camaro";
+//        cars[1] = "Corvette";
+//        cars[2] = "Tesla";
+//
+//        System.out.println(cars[0]);
+//
+//        for (int i = 0; i < cars.length; i++) {
+//            System.out.println(cars[i]);
+//        }
+//        --------------------------------------------------------------------------------------------------------------
     }
 }
