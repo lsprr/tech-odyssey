fe7db8e27388fa5677d9b105c043822ddc72b137
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-07 21:06:14 -0400

    Static Keyword

--- java/src/Main.java
+++ java/src/Main.java
@@ -534,6 +534,18 @@ public class Main {
 //        garage.park(car1);
 //        garage.park(car2);
 //        --------------------------------------------------------------------------------------------------------------
+//        Static Keyword
+//
+//        A single copy of a variable/method is created and shared.
+//        The class "owns" the static member
+//
+//        Friend friend1 = new Friend("Spongebob");
+//        Friend friend2 = new Friend("Patrick");
+//        Friend friend3 = new Friend("Squidward");
+//
+//        System.out.println(Friend.numberOfFriends);
+//        Friend.displayFriends();
+//        --------------------------------------------------------------------------------------------------------------
     }
 }
 
