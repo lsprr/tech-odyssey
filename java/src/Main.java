dca15760150a721dbbb80c5bf3c1c26fc0fcce1d
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-07 21:30:35 -0400

    Super Keyword

--- java/src/Main.java
+++ java/src/Main.java
@@ -563,6 +563,20 @@ public class Main {
 //        Dog dog = new Dog();
 //        dog.speak();
 //        --------------------------------------------------------------------------------------------------------------
+//        Super Keyword
+//
+//        Super Keyword refers to the superclass (parent) of an object
+//        Very similar to the "this" keyword
+//
+//        Hero hero1 = new Hero("Superman", 43, "everything");
+//        Hero hero2 = new Hero("Batman", 42, "$$$");
+//
+//        System.out.println(hero1.name);
+//        System.out.println(hero1.age);
+//        System.out.println(hero1.power);
+//
+//        System.out.println(hero2.toString());
+//        --------------------------------------------------------------------------------------------------------------
 
     }
 }
