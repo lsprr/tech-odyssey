9b542d451149c2d5d13c8a827416145f5ada65b7
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-07 20:10:38 -0400

    Array of Objects

--- java/src/Main.java
+++ java/src/Main.java
@@ -509,6 +509,23 @@ public class Main {
 //        System.out.println(car.toString()); # Explicitly
 //        System.out.println(car); # Implicitly
 //        --------------------------------------------------------------------------------------------------------------
+//        Array of Objects
+//
+//        Food[] refrigerator = new Food[3];
+//
+//        Food food1 = new Food("pizza");
+//        Food food2 = new Food("hamburger");
+//        Food food3 = new Food("hot-dog");
+//
+//        Food[] refrigerator = {food1, food2, food3};
+//
+//        refrigerator[0] = food1;
+//        refrigerator[1] = food2;
+//        refrigerator[2] = food3;
+//
+//        System.out.println(refrigerator[0].name);
+//        --------------------------------------------------------------------------------------------------------------
+
     }
 }
 
