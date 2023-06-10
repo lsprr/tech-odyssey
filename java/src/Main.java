bd5ee9655ef17a14716054c5a048e22a927b69ce
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-10 19:35:42 -0400

    Exception Handling

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -680,6 +681,33 @@ public class Main {
 //            animal.speak();
 //        }
 //        --------------------------------------------------------------------------------------------------------------
+//        Exception Handling
+//
+//        An event that occurs during the execution of a program that,
+//        disrupts the normal flow of instructions
+//
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            System.out.println("Enter a whole number to divide: ");
+            int x = scanner.nextInt();
+
+            System.out.println("Enter a whole number to divide by: ");
+            int y = scanner.nextInt();
+
+            int z = x / y;
+
+            System.out.println("result: " + z);
+        } catch (ArithmeticException e) {
+            System.out.println("You can't divide by zero");
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a number");
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        } finally {
+            scanner.close();
+        }
     }
 }
 
