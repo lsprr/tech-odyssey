4a27fa230942b6bb0f67d797fd520f94c17d8408
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-11 19:34:40 -0400

    More Labels

--- java/src/Main.java
+++ java/src/Main.java
@@ -849,6 +849,45 @@ public class Main {
 //        frame.add(label);
 //        frame.pack();
 //        --------------------------------------------------------------------------------------------------------------
+//        JPanel
+//
+//        A GUI component that functions as a container to hold other components
+//
+//        ImageIcon icon = new ImageIcon("thumbsup.png");
+//
+//        JLabel label = new JLabel();
+//        label.setText("Hi");
+//        label.setIcon(icon);
+//        label.setVerticalAlignment(JLabel.BOTTOM);
+//        label.setHorizontalAlignment(JLabel.RIGHT);
+//        label.setBounds(100, 100, 75, 75);
+//
+//        JPanel redPanel = new JPanel();
+//        redPanel.setBackground(Color.RED);
+//        redPanel.setBounds(0, 0, 250, 250);
+//        redPanel.setLayout(null);
+//
+//        JPanel bluePanel = new JPanel();
+//        bluePanel.setBackground(Color.BLUE);
+//        bluePanel.setBounds(250, 0, 250, 250);
+//        bluePanel.setLayout(null);
+//
+//        JPanel greenPanel = new JPanel();
+//        greenPanel.setBackground(Color.GREEN);
+//        greenPanel.setBounds(0, 250, 500, 250);
+//        greenPanel.setLayout(null);
+//
+//        JFrame frame = new JFrame();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setLayout(null);
+//        frame.setSize(750, 750);
+//        frame.setVisible(true);
+//        greenPanel.add(label);
+//        frame.add(redPanel);
+//        frame.add(bluePanel);
+//        frame.add(greenPanel);
+//        --------------------------------------------------------------------------------------------------------------
+
     }
 }
 
