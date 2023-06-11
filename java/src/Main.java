6cdb47b13a693143e627dc13670c5712845e1a56
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-10 20:06:24 -0400

    FileReader

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -739,6 +737,24 @@ public class Main {
 //            e.printStackTrace();
 //        }
 //        --------------------------------------------------------------------------------------------------------------
+//        FileReader
+//
+//        try {
+//            FileReader reader = new FileReader("art.txt");
+//            int data = reader.read();
+//
+//            while(data != -1) {
+//                System.out.print((char)data);
+//                data = reader.read();
+//            }
+//
+//            reader.close();
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        --------------------------------------------------------------------------------------------------------------
+
     }
 }
 
