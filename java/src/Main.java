172accba0fcadcb223791c9c5d99676abad4a312
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-11 19:17:40 -0400

    Labels

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,6 @@
 import javax.sound.sampled.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.io.*;
 import java.util.InputMismatchException;
@@ -818,7 +819,36 @@ public class Main {
 //
 //        MyFrame myFrame = new MyFrame();
 //        --------------------------------------------------------------------------------------------------------------
-
+//        JLabels
+//
+//        a GUI display area for a string of text, an image, or both
+//
+//        ImageIcon imageIcon = new ImageIcon("pic.png");
+//        Border border = BorderFactory.createLineBorder(Color.GREEN, 33);
+//
+//        JLabel label = new JLabel(); //create a label
+//        label.setText("Luis, do you even code?"); //set text of label
+//        label.setIcon(imageIcon);
+//        label.setHorizontalTextPosition(JLabel.CENTER); //set text LEFT, CENTER, RIGHT of imageIcon
+//        label.setVerticalTextPosition(JLabel.TOP); //set text TOP, CENTER, BOTTOM of imageIcon
+//        label.setForeground(new Color((0, 0, 0)); //set font color of text
+//        label.setFont(new Font("MV Boli", Font.PLAIN, 20)); //set font of text
+//        label.setIconTextGap(-25); //set gap of text to image
+//        label.setBackground(Color.BLACK); //set background color
+//        label.setOpaque(true); // display background color
+//        label.setBorder(border);
+//        label.setVerticalAlignment(JLabel.CENTER); //set vertical position of icon + text within label
+//        label.setHorizontalAlignment(JLabel.CENTER); //set horizontal position of icon + text within label
+//        label.setBounds(100, 100, 350, 350); //set x, y position within frame as well as dimensions
+//
+//        JFrame frame = new JFrame();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(500, 500);
+//        frame.setLayout(null);
+//        frame.setVisible(true);
+//        frame.add(label);
+//        frame.pack();
+//        --------------------------------------------------------------------------------------------------------------
     }
 }
 
