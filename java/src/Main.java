f6e4136adcf51368e9f91ae5b117b4bc3cfe50c5
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-11 20:09:14 -0400

    BorderLayout

--- java/src/Main.java
+++ java/src/Main.java
@@ -893,6 +893,41 @@ public class Main {
 //
 //        new MyFrame();
 //        --------------------------------------------------------------------------------------------------------------
+//        BorderLayout
+//
+//        A BorderLayout places components in five areas: NORTH, SOUTH, WEST, EAST, CENTER.
+//        All extra space is placed in the center area
+//
+//        JFrame frame = new JFrame();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(500, 500);
+//        frame.setLayout(new BorderLayout(10, 10));
+//        frame.setVisible(true);
+//
+//        JPanel panel1 = new JPanel();
+//        JPanel panel2 = new JPanel();
+//        JPanel panel3 = new JPanel();
+//        JPanel panel4 = new JPanel();
+//        JPanel panel5 = new JPanel();
+//
+//        panel1.setBackground(Color.RED);
+//        panel2.setBackground(Color.GREEN);
+//        panel3.setBackground(Color.YELLOW);
+//        panel4.setBackground(Color.MAGENTA);
+//        panel5.setBackground(Color.BLUE);
+//
+//        panel1.setPreferredSize(new Dimension(100, 100));
+//        panel2.setPreferredSize(new Dimension(100, 100));
+//        panel3.setPreferredSize(new Dimension(100, 100));
+//        panel4.setPreferredSize(new Dimension(100, 100));
+//        panel5.setPreferredSize(new Dimension(100, 100));
+//
+//        frame.add(panel1, BorderLayout.NORTH);
+//        frame.add(panel2, BorderLayout.WEST);
+//        frame.add(panel3, BorderLayout.EAST);
+//        frame.add(panel4, BorderLayout.SOUTH);
+//        frame.add(panel5, BorderLayout.CENTER);
+//        --------------------------------------------------------------------------------------------------------------
 
     }
 }
