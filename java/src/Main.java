f34875bf2a7282b4e2ac48438656c0260c730b76
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-12 11:27:18 -0400

    LayeredPane

--- java/src/Main.java
+++ java/src/Main.java
@@ -1009,6 +1009,41 @@ public class Main {
 //
 //        frame.setVisible(true);
 //        --------------------------------------------------------------------------------------------------------------
+//        JLayeredPane
+//
+//        Swing container that provides a third dimension for positioning components
+//        ex. depth, Z-index
+//
+//        JLabel label1 = new JLabel();
+//        label1.setOpaque(true);
+//        label1.setBackground(Color.RED);
+//        label1.setBounds(50, 50, 200, 200);
+//
+//        JLabel label2 = new JLabel();
+//        label2.setOpaque(true);
+//        label2.setBackground(Color.GREEN);
+//        label2.setBounds(100, 100, 200, 200);
+//
+//        JLabel label3 = new JLabel();
+//        label3.setOpaque(true);
+//        label3.setBackground(Color.BLUE);
+//        label3.setBounds(150, 150, 200, 200);
+//
+//        JLayeredPane layeredPane = new JLayeredPane();
+//        layeredPane.setBounds(0, 0, 500, 500);
+//
+//        layeredPane.add(label1, Integer.valueOf(0));
+//        layeredPane.add(label2, Integer.valueOf(0));
+//        layeredPane.add(label3, Integer.valueOf(1));
+//
+//        JFrame frame = new JFrame("JLayeredPane");
+//        frame.add(layeredPane);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(new Dimension(500, 500));
+//        frame.setLayout(null);
+//        frame.setVisible(true);
+//        --------------------------------------------------------------------------------------------------------------
+
 
     }
 }
