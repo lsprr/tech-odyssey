7993ffc651d2b6ae54441c318b4694f77e01e2f4
lsprr <16653744+lsprr@users.noreply.github.com>
2023-06-12 12:14:33 -0400

    JOptionPane

--- java/src/Main.java
+++ java/src/Main.java
@@ -1047,7 +1047,58 @@ public class Main {
 //
 //        LaunchPage launchPage = new LaunchPage();
 //        --------------------------------------------------------------------------------------------------------------
-
+//        JOptionPane
+//
+//        Pop up a standard dialog box that prompts users for a value or informs them of something
+//
+//        JOptionPane.showMessageDialog(
+//                null,
+//                "This is some useless info",
+//                "title", JOptionPane.PLAIN_MESSAGE
+//        );
+//        JOptionPane.showMessageDialog(
+//                null,
+//                "This is some useless info",
+//                "title", JOptionPane.INFORMATION_MESSAGE
+//        );
+//        JOptionPane.showMessageDialog(
+//                null,
+//                "This is some useless info",
+//                "title", JOptionPane.QUESTION_MESSAGE
+//        );
+//        JOptionPane.showMessageDialog(
+//                null,
+//                "This is some useless info",
+//                "title", JOptionPane.WARNING_MESSAGE
+//        );
+//        JOptionPane.showMessageDialog(
+//                null,
+//                "This is some useless info",
+//                "title", JOptionPane.ERROR_MESSAGE
+//        );
+//
+//        int answer = JOptionPane.showConfirmDialog(
+//                null,
+//                "Luis, do you even code?",
+//                "This is my title",
+//                JOptionPane.YES_NO_CANCEL_OPTION
+//        );
+//        String name = JOptionPane.showInputDialog("What is your name? :");
+//        System.out.println("Hello " + name);
+//
+//        String[] responses = {"No, you're awesome!", "Thank you!", "*Blush*"}
+//        ImageIcon icon = new ImageIcon("smile.png");
+//        JOptionPane.showOptionDialog(
+//                null,
+//                "You are awesome!",
+//                "secret message",
+//                JOptionPane.YES_NO_CANCEL_OPTION,
+//                JOptionPane.INFORMATION_MESSAGE,
+//                icon,
+//                responses,
+//                0
+//        );
+//        --------------------------------------------------------------------------------------------------------------
     }
 }
 
